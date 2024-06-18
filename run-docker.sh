d4c933b72c23927291b53c3cfe097a9ad65e88b4
William Ingold <wingold@cs.umd.edu>
2024-06-17 22:29:18 -0400

    Set uid, gid and user name. Redirect error on status to not display.

--- run-docker.sh
+++ run-docker.sh
@@ -4,8 +4,25 @@ NAME=${1:-"league_predictor"}
 TAG=${2:-"latest"}
 CONTAINER_NAME=$NAME
 
-docker build -t ${NAME}:${TAG} -f ./Dockerfile .
+if [ $GID ]; then
+    GROUP_ID=$GID
+else
+    GROUP_ID=1001
+fi
 
-if [ "$( docker container inspect -f '{{.State.Status}}' $CONTAINER_NAME)" != "running" ]; then
+docker build \
+    -t ${NAME}:${TAG} \
+    -f ./Dockerfile \
+    --build-arg="USER_ID=${UID}" \
+    --build-arg="GROUP_ID=${GROUP_ID}" \
+    --build-arg="GROUP_NAME=summoner" \
+    --build-arg="USER_NAME='summoner'" \
+    .
+
+status="$( docker container inspect -f '{{.State.Status}}' ${CONTAINER_NAME} 2> /dev/null )"
+
+if [ -z $status ]; then
     docker run --name ${CONTAINER_NAME} -d ${NAME}:${TAG} 
+elif [ $status = "exited" ]; then
+    docker start ${CONTAINER_NAME}
 fi
